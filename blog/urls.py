8d898b387a2730f4122488d746094be17d5b9372
gukehan <854091529@qq.com>
2021-06-06 17:13:48 +0800

    modify setting

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/<int:pk>/', views.detail, name='detail'),
+    path('search/', views.search, name='search'),
+]
---
+++ blog/views.py
@@ -0,0 +1,39 @@
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from markdown.extensions.toc import TocExtension, slugify
+from django.contrib import messages
+from .models import Post
+import markdown
+import re
+
+def search(request):
+    q = request.GET.get('q')
+    if not q:
+        error_msg = "请输入搜索关键词"
+        messages.add_message(request, messages.ERROR, error_msg, extra_tags='danger')
+        return redirect('blog:index')
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'post_list': post_list})
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+# Create your views here.
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+
+    return render(request, 'blog/detail.html', context={'post': post})
